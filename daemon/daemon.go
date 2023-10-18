214b9316188e7a5bb68c3730dcfc955fb0928d12
Bruce Eckel <mindviewinc@gmail.com>
2023-10-18 04:22:01 -0600

    Example and blog

---
+++ daemon/daemon.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func accumulate(s string, n int) int {
+	sum := 0
+	for ; n > 0; n-- {
+		sum += n
+		fmt.Printf("%s: %d\n", s, sum)
+	}
+	return sum
+}
+
+func main() {
+	fmt.Printf("Total: %d\n", accumulate("A", 5))
+	go accumulate("B", 4)
+	time.Sleep(100 * time.Millisecond)
+}
